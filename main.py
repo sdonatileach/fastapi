b8afbf670042cc562053654a63a3d05670dac9e3
EC2 Default User <ec2-user@ip-172-31-6-9.ec2.internal>
2021-11-13 02:27:50 +0000

    formatted with black

--- main.py
+++ main.py
@@ -7,36 +7,38 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-	return {"message": "Add to the url /NSFW for a Not Safe for Work joke or /SFW for a Safe For Work joke.  Alternatively, add /docs to the url for the joke UI."}
+    return {
+        "message": "Add to the url /NSFW for a Not Safe for Work joke or /SFW for a Safe For Work joke.  Alternatively, add /docs to the url for the joke UI."
+    }
 
 
 @app.get("/NSFW")
 async def NSFW():
 
-	any_joke = "https://v2.jokeapi.dev/joke/Any"
-	response_any = requests.get(any_joke)
+    any_joke = "https://v2.jokeapi.dev/joke/Any"
+    response_any = requests.get(any_joke)
 
-	if "joke" in response_any.json():
-		return response_any.json()["joke"]
-	else:
-		return "{} {}".format(
-			response_any.json()["setup"], response_any.json()["delivery"]
-		)
+    if "joke" in response_any.json():
+        return response_any.json()["joke"]
+    else:
+        return "{} {}".format(
+            response_any.json()["setup"], response_any.json()["delivery"]
+        )
 
 
 @app.get("/SFW")
 async def SFW():
 
-	sfw_joke = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
-	response_sfw = requests.get(sfw_joke)
+    sfw_joke = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    response_sfw = requests.get(sfw_joke)
 
-	if "joke" in response_sfw.json():
-		return response_sfw.json()["joke"]
-	else:
-		return "{} {}".format(
-			response_sfw.json()["setup"], response_sfw.json()["delivery"]
-		)
+    if "joke" in response_sfw.json():
+        return response_sfw.json()["joke"]
+    else:
+        return "{} {}".format(
+            response_sfw.json()["setup"], response_sfw.json()["delivery"]
+        )
 
 
 if __name__ == "__main__":
-	uvicorn.run(app, port=8080, host="0.0.0.0")
+    uvicorn.run(app, port=8080, host="0.0.0.0")
--- test_main.py
+++ test_main.py
@@ -1,27 +1,27 @@
 import requests
 
 def Ping():
-    # NSFW
-    any_joke = "https://v2.jokeapi.dev/joke/Any"
-    response_any = requests.get(any_joke)  
+	# NSFW
+	any_joke = "https://v2.jokeapi.dev/joke/Any"
+	response_any = requests.get(any_joke)  
+	print(response_any)
 
-
-    # SFW
-    sfw_joke = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
-    response_sfw = requests.get(sfw_joke)
-    
-    try:
-        if "joke" in response_any.json():
-            print(response_any.json()["joke"])
-        else:
-            print("{} {}".format(response_any.json()["setup"], response_any.json()["delivery"]))
-            
-        if "joke" in response_sfw.json():
-            print(response_sfw.json()["joke"])
-        else:
-            print("{} {}".format(response_sfw.json()["setup"], response_sfw.json()["delivery"]))    
-    except ValueError:        
-        return "Link is unavailable"
-        
+	# SFW
+	sfw_joke = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+	response_sfw = requests.get(sfw_joke)
+	
+	try:
+		if "joke" in response_any.json():
+			print(response_any.json()["joke"])
+		else:
+			print("{} {}".format(response_any.json()["setup"], response_any.json()["delivery"]))
+			
+		if "joke" in response_sfw.json():
+			print(response_sfw.json()["joke"])
+		else:
+			print("{} {}".format(response_sfw.json()["setup"], response_sfw.json()["delivery"]))    
+	except ValueError:        
+		return "Link is unavailable"
+		
 def test_link_check():    
-    assert Ping() != 'Link is unavailable'
+	assert Ping() != 'Link is unavailable'
